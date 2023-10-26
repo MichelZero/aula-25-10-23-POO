db001e70247552dad144f24f07f6a15b9cedaa21
Michel <106674127+MichelZero@users.noreply.github.com>
2023-10-26 10:41:07 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,32 @@
+# Criar duas classes, A e B:
+# Sendo que a classe B tem um relacionamento de Composição 
+# com a classe A, para isso inicialize o objeto A no método somaTodosNum
+
+
+#Composição
+#class A
+class A:
+  def __init__(self, a, b, c):
+    self.a = a
+    self.b = b
+    self.c = c
+
+class B:
+  def __init__(self, d, e,):
+    self.d = d
+    self.e = e
+    self.A = A("soma = ", 2, 8)
+
+  def somaTodosNum(self):
+    x = self.d + self.e + self.A.b + self.A.c
+    return x
+  
+  
+  
+  
+  
+##############################################################
+# usando as classes A e B
+b1 = B(8, 10)
+# imprimir a composição de B e A
+print(f"{b1.A.a} {b1.somaTodosNum()}")
